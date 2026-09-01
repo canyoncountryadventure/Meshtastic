16e71521464ca663d83e608ff509952a428d146f
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-08-31 23:18:10 -0600

    Add AthleteOS layout

---
+++ athleteos-build/app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'AthleteOS',description:'Private athlete health and performance dashboard'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
