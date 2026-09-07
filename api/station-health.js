2da9352d452c6f98d2bb84db6cc84d3a9847327f
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-09-07 13:41:33 -0600

    Add It's a Swell Day station health

--- api/station-health.js
+++ api/station-health.js
@@ -4,6 +4,7 @@ const STATIONS = [
   { node: 1436900584, name: 'Hidden Valley Repeater', battery: true },
   { node: 2740603892, name: 'Heltec Home', battery: false },
   { node: 1577197109, name: 'Fishlake Hightop', battery: true },
+  { node: 1949224949, name: "It's a Swell Day", battery: true },
 ];
 const EXPECTED_INTERVAL_MINUTES = 60;
 const ALERT_AFTER_MINUTES = 195;
